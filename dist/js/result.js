c263d15562634360921073dbc1553244f745a812
Aman <7767221+raiaman15@users.noreply.github.com>
2020-10-01 20:09:11 +0530

    enhancements

--- dist/js/result.js
+++ dist/js/result.js
@@ -23,44 +23,44 @@ var examProperties = {
 
 var questions = [
     {
-        question: "_______ is the practice and precautions taken to protect valuable information from unauthorized access, recording, disclosure or destruction.",
-        options: ["Network Security", "Database Security", "Information Security", "Physical Security"]
+        question: '_______ is the practice and precautions taken to protect valuable information from unauthorized access, recording, disclosure or destruction.',
+        options: ['Network Security', 'Database Security', 'Information Security', 'Physical Security']
     },
     {
-        question: "From the options below, which of them is not a threat to information security?",
-        options: ["Disaster", "Eavesdropping", "Information leakage", "Unchanged default password"]
+        question: 'From the options below, which of them is not a threat to information security?',
+        options: ['Disaster', 'Eavesdropping', 'Information leakage', 'Unchanged default password']
     },
     {
-        question: "From the options below, which of them is not a vulnerability to information security?",
-        options: ["flood", "without deleting data, disposal of storage media", "unchanged default password", "latest patches and updates not done"]
+        question: 'From the options below, which of them is not a vulnerability to information security?',
+        options: ['flood', 'without deleting data, disposal of storage media', 'unchanged default password', 'latest patches and updates not done']
     },
     {
-        question: "_______ platforms are used for safety and protection of information in the cloud.",
-        options: ["Cloud workload protection platforms", "Cloud security protocols", "AWS", "One Drive"]
+        question: '_______ platforms are used for safety and protection of information in the cloud.',
+        options: ['Cloud workload protection platforms', 'Cloud security protocols', 'AWS', 'One Drive']
     },
     {
-        question: "Which of the following information security technology is used for avoiding browser-based hacking?",
-        options: ["Anti-malware in browsers", "Remote browser access", "Adware remover in browsers", "Incognito mode in a browser"]
+        question: 'Which of the following information security technology is used for avoiding browser-based hacking?',
+        options: ['Anti-malware in browsers', 'Remote browser access', 'Adware remover in browsers', 'Incognito mode in a browser']
     },
     {
-        question: "The full form of EDR is _______",
-        options: ["Endpoint Detection and recovery", "Early detection and response", "Endpoint Detection and response", "Endless Detection and Recovery"]
+        question: 'The full form of EDR is _______',
+        options: ['Endpoint Detection and recovery', 'Early detection and response', 'Endpoint Detection and response', 'Endless Detection and Recovery']
     },
     {
-        question: "_______ technology is used for analyzing and monitoring traffic in network and information flow.",
-        options: ["Cloud access security brokers (CASBs)", "Managed detection and response (MDR)", "Network Security Firewall", "Network traffic analysis (NTA)"]
+        question: '_______ technology is used for analyzing and monitoring traffic in network and information flow.',
+        options: ['Cloud access security brokers (CASBs)', 'Managed detection and response (MDR)', 'Network Security Firewall', 'Network traffic analysis (NTA)']
     },
     {
-        question: "Compromising confidential information comes under _______",
-        options: ["Bug", "Threat", "Vulnerability", "Attack"]
+        question: 'Compromising confidential information comes under _______',
+        options: ['Bug', 'Threat', 'Vulnerability', 'Attack']
     },
     {
-        question: "Lack of access control policy is a _______",
-        options: ["Bug", "Threat", "Vulnerability", "Attack"]
+        question: 'Lack of access control policy is a _______',
+        options: ['Bug', 'Threat', 'Vulnerability', 'Attack']
     },
     {
-        question: "Possible threat to any information cannot be _______",
-        options: ["reduced", "transferred", "protected", "ignored"]
+        question: 'Possible threat to any information cannot be _______',
+        options: ['reduced', 'transferred', 'protected', 'ignored']
     },
 ]
 
@@ -111,9 +111,7 @@ var userPreviousLog = [
 
 ]
 
-var myResponse = {
-    response: [0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4]
-}
+var myResponse = [0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4]
 
 var otherResponse = [
     {
@@ -122,19 +120,19 @@ var otherResponse = [
     },
     {
         user: 'Anshuman',
-        response: [0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4]
+        response: [1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0]
     },
     {
         user: 'Kritarth',
-        response: [0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4]
+        response: [2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1]
     },
     {
         user: 'Vikram',
-        response: [0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4]
+        response: [3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2]
     },
     {
         user: 'Paras',
-        response: [0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4]
+        response: [4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3]
     },
 ]
 
@@ -144,86 +142,71 @@ var correctResponse = {
     response: [0, 1, 2, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4]
 }
 
-function createDataset() {
-    otherResponse.forEach()
+function analysePerformance() {
+    topics = new Set(correctResponse.topic)
+    qc = correctResponse.topic.length
+    for (i = 0; i < qc; i++) {
+        if (myResponse[i] == correctResponse.response[i]) {
+            console.log('Correct' + i)
+        }
+    }
+}
+
+function createOthersDataset(d) {
+    data = {
+        label: d.user,
+        data: d.response,
+        borderColor: 'rgba(100,100,100,0.1)',
+        fill: false,
+        lineTension: 0
+    }
+    datasets.push(data)
 }
 
 function initChart() {
-
-    var lineChartData = {
-        labels: ['Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', 'Q7', 'Q8', 'Q9', 'Q10', 'Q11', 'Q12', 'Q13', 'Q14', 'Q15', 'Q16', 'Q17', 'Q18', 'Q19', 'Q20', 'Q21', 'Q22', 'Q23', 'Q24', 'Q25'],
-        datasets: [
-            {
-                label: 'My Response',
-                backgroundColor: 'white',
-                borderColor: 'grey',
-                data: [0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4],
-                lineTension: 0
-            },
-            {
-                label: 'Anonymous 1 Response',
-                backgroundColor: 'white',
-                borderColor: 'rgba(210, 214, 222, .25)',
-                data: [1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0],
-                lineTension: 0
-            },
-            {
-                label: 'Anonymous 2 Response',
-                backgroundColor: 'white',
-                borderColor: 'rgba(210, 214, 222, .25)',
-                data: [2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1],
-                lineTension: 0
-            },
-            {
-                label: 'Anonymous 3 Response',
-                backgroundColor: 'white',
-                borderColor: 'rgba(210, 214, 222, .25)',
-                data: [1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0],
-                lineTension: 0
-            },
-            {
-                label: 'Anonymous 4 Response',
-                backgroundColor: 'white',
-                borderColor: 'rgba(210, 214, 222, .25)',
-                data: [2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1],
-                lineTension: 0
-            },
-            {
-                label: 'Correct Response',
-                backgroundColor: 'white',
-                borderColor: 'green',
-                data: [1, 12, 2, 2, 3, 3, 4, 4, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 1, 1, 2, 2, 3, 3, 4],
-                lineTension: 0
-            },
-        ]
-    }
-
-    var lineChartOptions = {
-        maintainAspectRatio: false,
-        responsive: true,
-        legend: { display: true },
-        scales: {
-            xAxes: [{ gridLines: { display: false, } }],
-            yAxes: [{ gridLines: { display: false, }, ticks: { stepSize: 1 } }]
-        }
-    }
-
-    //------------------------------
-    //- CLASSMATE COMPARISON CHART -
-    //------------------------------
-    var lineChartCanvas = $('#lineChart').get(0).getContext('2d')
-    lineChartData.datasets[0].fill = false;
-    lineChartData.datasets[1].fill = false;
-    lineChartOptions.datasetFill = false
-
-    var lineChart = new Chart(lineChartCanvas, {
+    otherResponse.forEach(createOthersDataset)
+    datasets.push({
+        label: student,
+        data: myResponse,
+        borderColor: 'rgba(100,100,255,0.5)',
+        fill: false,
+        lineTension: 0
+    })
+    datasets.push({
+        label: 'Correct Response',
+        data: correctResponse.response,
+        borderColor: 'rgba(100,255,100,0.5)',
+        fill: false,
+        lineTension: 0
+    })
+    var ctx = $('#lineChart').get(0).getContext('2d');
+    var myLine = new Chart(ctx, {
         type: 'line',
-        data: lineChartData,
-        options: lineChartOptions
+        data: {
+            xLabels: correctResponse.topic,
+            yLabels: ['0', '1', '2', '3', '4', '12', '13', '14', '23', '24', '34', '123', '124', '134', '234', '1234'],
+            datasets: datasets
+        },
+        options: {
+            maintainAspectRatio: false,
+            responsive: true,
+            legend: { display: false },
+            scales: {
+                xAxes: [{
+                    display: true,
+                    gridLines: { display: false, },
+                    scaleLabel: { display: true, labelString: 'Month' }
+                }],
+                yAxes: [{
+                    type: 'category',
+                    scaleLabel: { display: true, labelString: 'Request State' },
+                    ticks: { reverse: true },
+                }]
+            }
+        }
     })
 }
 
-//
 function fillData() {
     for (const entry of entries) {
         if (entry[0] == 'student') { student = entry[1] }
@@ -235,6 +218,7 @@ function fillData() {
 
 // Document on Ready
 $(document).ready(function () {
+    analysePerformance()
     initChart()
     fillData()
 })
